24836d02d7d200f5e647d8102e1d71c5a41237ff
Tomás Martins <tomasfilipe7@ua.pt>
2020-05-25 18:46:10 +0100

    NewsFeed

---
+++ ClubMember/NewsFeed.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClubMember.Images
+{
+    /// <summary>
+    /// Interaction logic for NewsFeed.xaml
+    /// </summary>
+    public partial class NewsFeed : Page
+    {
+        public NewsFeed()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(PageManager.pagemanager.getSettings());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(PageManager.pagemanager.getProfile());
+        }
+
+        private void Button_Click_Shopping(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(PageManager.pagemanager.GetShopOptions());
+        }
+
+    }
+}
